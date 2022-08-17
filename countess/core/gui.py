97df508e9b49fdd8f43efc1ca75b6fe9133f58f9
Nick Moore <nick@zoic.org>
2022-08-17 21:31:10 +1000

    progress on gui rethink

--- countess/core/gui.py
+++ countess/core/gui.py
@@ -3,9 +3,12 @@ import threading
 import tkinter as tk
 import tkinter.ttk as ttk
 import ttkthemes # type:ignore
+from tkinter import filedialog
+import pathlib
 
 from typing import Optional
 from collections.abc import Iterable, Sequence
+from functools import partial
 
 from tkinter.scrolledtext import ScrolledText
 
@@ -60,8 +63,12 @@ class PluginFrameWrapper(_PluginFrameWrapper):
                 ee.grid(row=n+2, column=1, sticky="ew")
 
         self.last_row = len(self.plugin_selected.params)+2
+
+        self.add_file_button = ttk.Button(self.subframe, text="Add File", command=self.add_file_clicked)
+        self.add_file_button.grid(row=self.last_row, column=0)
+
         self.button = tk.Button(self.subframe, text="Run", command=self.run_clicked)
-        self.button.grid(row=self.last_row, columnspan=2)
+        self.button.grid(row=self.last_row, column=1)
 
         self.subframe.pack(expand=True, pady=10, padx=10)
 
@@ -72,6 +79,20 @@ class PluginFrameWrapper(_PluginFrameWrapper):
         self.button['state'] = 'disabled'
         t = threading.Thread(target=self.run)
         t.start()
+
+    def add_file_clicked(self, *args):
+        self.add_file_button['state'] = 'disabled'
+        filenames = filedialog.askopenfilenames()
+        for n, filename in enumerate(filenames):
+            filestem = pathlib.Path(filename).stem
+            ttk.Label(self.subframe, text=filename).grid(row=10+n,column=0)
+            e = ttk.Entry(self.subframe)
+            e.grid(row=10+n,column=1)
+            e.insert(0,filestem)
+
+
+
+        print(filename)
 
     def run(self):
    
@@ -140,6 +161,167 @@ class ProgressWindow:
         self.pbar['value'] = 100
 
 
+# These classes specialize widgets instead of wrapping them wwhich isn't my favourite way
+# but it seems to be clearer in this case.
+# The Tkinter "master" terminology is also objectionable.
+
+class CancelButton(tk.Button):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, text="\u274c", width=1, highlightthickness=0, bd=0, fg="red", **kwargs)
+
+class PluginWrapper(ttk.Frame):
+    def __init__(self, master, plugin):
+        super().__init__(master)
+        self.plugin = plugin
+
+        #ttk.Label(self, text=self.plugin.description).pack()
+        #ttk.Separator(self, orient="horizontal").pack(fill='x')
+        self.header = PluginHeader(self, plugin)
+        self.config = PluginConfigurator(self, plugin)
+        self.footer = PluginFooter(self, plugin)
+        for w in self.winfo_children(): w.pack(fill=tk.X)
+        CancelButton(self, command=self.delete).place(anchor=tk.NE, relx=1, rely=0)
+
+    def delete(self):
+        self.master.del_plugin(self)
+
+    def run(self):
+        self.footer.set_progress(42,107,"WOO")
+
+class PluginHeader(ttk.Frame):
+    def __init__(self, master, plugin):
+        super().__init__(master)
+        self.plugin = plugin
+        ttk.Label(self, text=self.plugin.name).pack()
+        ttk.Label(self, text=self.plugin.description).pack()
+        ttk.Separator(self, orient="horizontal").pack()
+    
+class PluginConfigurator(ttk.Frame):
+    def __init__(self, master, plugin):
+        super().__init__(master)
+        self.plugin = plugin
+
+        self.param_entries = {}
+        if self.plugin.params is not None:
+            for n, (k, v) in enumerate(self.plugin.params.items()):
+                ttk.Label(self, text=v['label']).grid(row=n, column=0)
+                ee = tk.Entry(self)
+                self.param_entries[k] = ee
+                ee.grid(row=n, column=1, sticky="ew")
+
+
+class LabeledProgressbar(ttk.Progressbar):
+
+    style_data = [
+        ('LabeledProgressbar.trough', {
+            'children': [
+                ('LabeledProgressbar.pbar', {'side': 'left', 'sticky': 'ns'}),
+                ("LabeledProgressbar.label", {"sticky": ""})
+            ],
+            'sticky': 'nswe'
+        })
+    ]
+
+    def __init__(self, master, *args, **kwargs):
+        self.style = ttk.Style(master)
+        # make up a new style name so we don't interfere with other LabeledProgressbars
+        # and accidentally change their color or label
+        self.style_name = f"_id_{id(self)}"
+        self.style.layout(self.style_name, self.style_data)
+        self.style.configure(self.style_name, background="green")
+
+        kwargs['style'] = self.style_name
+        super().__init__(master, *args, **kwargs)
+
+    def update_label(self, s):
+        self.style.configure(self.style_name, text=s)
+
+
+class PluginFooter(ttk.Frame):
+    def __init__(self, master, plugin):
+        super().__init__(master)
+        self.plugin = plugin
+
+        ttk.Separator(self, orient="horizontal").pack(fill='x')
+        tk.Button(self, text="RUN", fg="green", command=master.run).pack()
+        self.pbar = LabeledProgressbar(self, length=500)
+        self.pbar.pack()
+
+    def set_progress(self, a,b,s="Running"):
+        if b:
+            self.pbar.stop()
+            self.pbar['mode'] = 'determinate'
+            self.pbar['value'] = (100 * a / b)
+            self.pbar.update_label(f"{s} : {a} / {b}")
+        else:
+            self.pbar['mode'] = 'indeterminate'
+            self.pbar.start()
+            self.pbar.update_label(f"{s} : {a}" if a is not None else s)
+                
+class PluginChooser(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        for n, p in enumerate(plugin_manager.plugins):
+            ttk.Button(self, text=p.name, command=partial(self.choose, p)).grid(row=n, column=0)
+            ttk.Label(self, text=p.description).grid(row=n, column=1)
+
+    def choose(self, plugin):
+        self.master.add_plugin(plugin)
+
+        
+
+class PipelineBuilder(ttk.Notebook):
+    def __init__(self, master):
+        super().__init__(master)
+        self.enable_traversal()
+
+        self.add(PluginChooser(self), text="+ Add +")
+        self.pack()
+
+    def add_plugin(self, plugin):
+        plugin_wrapper = PluginWrapper(self, plugin)
+
+        index = self.index('end')-1
+        self.insert(index, plugin_wrapper, text=plugin.name, sticky="ew")
+        self.select(index)
+        plugin_wrapper.tab_id = self.select()
+
+    def del_plugin(self, plugin_wrapper):
+        self.forget(plugin_wrapper.tab_id)
+        self.select(self.index('end')-1)
+
+class DataFramePreview(ttk.Frame):
+    def __init__(self, master, ddf):
+        super().__init__(master)
+        self.ddf = ddf
+        self.height = 10
+        self.index_values = list(ddf.index)[0:1000]
+
+        self.treeview = ttk.Treeview(self, columns=list(ddf.columns), displaycolumns='#all', height=self.height, selectmode='none')
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.scroll_to)
+
+        self.treeview.pack(side="left")
+        self.scrollbar.pack(side="right", fill="y")
+
+        self.set_offset(0)
+
+    def scroll_to(self, _, x):
+        x = float(x)
+        if x<=0: x = 0
+        if x>=1: x = 1
+        self.set_offset(int((len(self.index_values)-self.height) * x))
+
+    def set_offset(self, offset):
+        # XXX horribly inefficient PoC
+        for n in range(0, self.height):
+            if self.treeview.exists(n): self.treeview.delete(n)
+        indices = self.index_values[offset:offset+self.height]
+        for n, (index, *values) in enumerate(self.ddf.loc[indices[0]:indices[-1]].itertuples()):
+            self.treeview.insert('', 'end', iid=n, text=index, values=values)
+
+
+
+
 class MainWindow:
     """The main plugin selection window.  This should be building a "chain" of plugins
     but for now it just has three tabs, for Input, Transform and Output plugins"""
@@ -165,13 +347,15 @@ def main():
     root = ttkthemes.ThemedTk()
     root.title('CountESS')
 
+
     themes = set(root.get_themes())
     for t in ['winnative', 'aqua', 'ubuntu', 'clam']:
         if t in themes:
             root.set_theme(t)
             break
 
-    MainWindow(root)
+    PipelineBuilder(root)
+    #MainWindow(root)
     root.mainloop()
 
 if __name__ == '__main__':
