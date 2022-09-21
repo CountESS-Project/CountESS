9f976d4b8953e5bafa99bb686fa1cccc97ba42f9
Nick Moore <nick@zoic.org>
2022-09-21 21:32:11 +1000

    update group by plugin

--- countess/plugins/group_by.py
+++ countess/plugins/group_by.py
@@ -4,11 +4,12 @@ from collections.abc import Iterable, Mapping
 import dask.dataframe as dd
 import numpy as np
 from countess.core.plugins import DaskTransformPlugin
-from countess.core.parameters import StringParam
+from countess.core.parameters import ChoiceParam
 
 
 class GroupByPlugin(DaskTransformPlugin):
     """Groups a Dask Dataframe by an arbitrary column and rolls up rows"""
+
     # XXX should support multiple operations but we'd need to support MultiIndex columns for that.
 
     name = "Group By"
@@ -16,17 +17,21 @@ class GroupByPlugin(DaskTransformPlugin):
     description = "XXX"
 
     parameters = {
-        'column': StringParam("Group By", default="Index", choices=[]),
-        'operation': StringParam("Operation", default="sum", choices=["sum", "size", "std", "var", "sem", "min", "max"])
+        "column": ChoiceParam("Group By", "Index", choices=[]),
+        "operation": ChoiceParam(
+            "Operation",
+            "sum",
+            choices=["sum", "size", "std", "var", "sem", "min", "max"],
+        ),
     }
 
     def update(self):
         input_columns = sorted(self.previous_plugin.output_columns())
-        self.parameters['column'].choices = ['Index'] + input_columns
+        self.parameters["column"].choices = ["Index"] + input_columns
 
     def run(self, ddf_in: dd.DataFrame):
-        col_name = self.parameters['column'].value
-        oper = self.parameters['operation'].value
-        col = ddf_in.index if col_name == 'Index' else ddf_in[col_name]
+        col_name = self.parameters["column"].value
+        oper = self.parameters["operation"].value
+        col = ddf_in.index if col_name == "Index" else ddf_in[col_name]
 
         return ddf_in.groupby(col).agg(oper)
