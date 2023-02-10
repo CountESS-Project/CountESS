90ebcbf8a2613370db54a81f8c4ebb1360db41df
Nick Moore <nick@zoic.org>
2023-02-10 23:38:53 +1100

    start on multi-way join

--- countess/plugins/join.py
+++ countess/plugins/join.py
@@ -9,10 +9,9 @@ import itertools
 from collections.abc import Callable
 from typing import Optional
 
-from countess.core.parameters import ChoiceParam, StringParam
+from countess import VERSION
+from countess.core.parameters import ChoiceParam, StringParam, ArrayParam, BooleanParam, MultiParam
 from countess.core.plugins import DaskBasePlugin, DaskProgressCallback
-
-VERSION = "0.0.1"
 
 
 INDEX = '— INDEX —'
@@ -26,43 +25,71 @@ class DaskJoinPlugin(DaskBasePlugin):
     version = VERSION
 
     parameters = {
-        "join_how": ChoiceParam("Join Direction", "outer", ["outer", "inner", "left", "right"]),
-        "left_on": ChoiceParam("Left Column", INDEX, choices = [INDEX]),
-        "right_on": ChoiceParam("Right Column", INDEX, choices = [INDEX]),
+        "inputs": ArrayParam("Inputs",
+            MultiParam("Input", {
+                "source": StringParam("Source", read_only=True),
+                "join_on": ChoiceParam("Join On", INDEX, choices = [INDEX]),
+                "required": BooleanParam("Required", True),
+            }), read_only=True
+        )
     }
 
-    @classmethod
-    def accepts(self, data) -> bool:
-        return (
-            type(data) is list and
-            isinstance(data[0], (dd.DataFrame, pd.DataFrame)) and
-            isinstance(data[1], (dd.DataFrame, pd.DataFrame))
-        ) or (
-            type(data) is dict and 
-            all((isinstance(d, (dd.DataFrame, pd.DataFrame)) for d in data.values()))
-        )
-
     def prepare(self, data):
-        if type(data) is dict:
-            data = list(data.values())
-        if len(data) >= 2:
-            self.parameters['left_on'].set_choices([INDEX] + list(data[1].columns))
-            self.parameters['right_on'].set_choices([INDEX] + list(data[0].columns))
 
+        print(data.keys())
+        if len(data.keys()) > 2:
+            raise NotImplementedError("Only two-way joins supported right now")
+
+        inputs_param = self.parameters["inputs"]
+
+        print(inputs_param.value)
+
+        found = set()
+        for n, sp in enumerate(inputs_param):
+            source = sp['source'].value
+            if isinstance(data.get(source), (pd.DataFrame, dd.DataFrame)):
+                print(f"FOUND {source}")
+                found.add(source)
+                sp['join_on'].choices = [ INDEX ] + list(data[source].columns)
+            else:
+                print(f"DEL {source}")
+                inputs_param.del_row(n)
+
+        for key, df in data.items():
+            if isinstance(df, (pd.DataFrame, dd.DataFrame)) and not key in found:
+                print(f"ADD {key}")
+                #sp = inputs_param.add_row()
+                #if sp:
+                #    sp['source'].value = key
+                #    sp['join_on'].choices = [ INDEX ] + list(df.columns )
+                
     def merge_dfs(self, prev_ddf: dd.DataFrame, this_ddf: dd.DataFrame) -> dd.DataFrame:
         """Merge the new data into the old data.  Only called
         if there is a previous plugin to merge data from."""
+        ip1 = self.parameters["inputs"][0]
+        ip2 = self.parameters["inputs"][1]
+        if ip1['required'].value:
+            if ip2['required'].value:
+                join_how = 'inner'
+            else:
+                join_how = 'left'
+        else:
+            if ip2['required'].value:
+                join_how = 'right'
+            else:
+                join_how = 'outer'
+
         join_params = {
-            "how": self.parameters['join_how'].value
+            "how": join_how,
         }
-        if self.parameters['left_on'].value == INDEX:
+        if ip1['join_on'].value == INDEX:
             join_params['left_index'] = True
         else:
-            join_params['left_on'] = self.parameters['left_on'].value 
-        if self.parameters['right_on'].value == INDEX:
+            join_params['left_on'] = ip1['join_on'].value
+        if ip2['join_on'].value == INDEX:
             join_params['right_index'] = True
         else:
-            join_params['right_on'] = self.parameters['right_on'].value 
+            join_params['right_on'] = ip2['join_on'].value
 
         return prev_ddf.merge(this_ddf, **join_params)
        
