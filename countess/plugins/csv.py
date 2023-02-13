7cc4284403d6b81c8a10129fde99098e5c5db4bc
Nick Moore <nick@zoic.org>
2023-02-13 15:12:22 +1100

    Better datatyping for csv plugin too

--- countess/plugins/csv.py
+++ countess/plugins/csv.py
@@ -6,12 +6,13 @@ import pandas as pd  # type: ignore
 import csv
 from typing import Any, Callable
 
-from countess.core.parameters import BooleanParam, ChoiceParam, FileParam, FileSaveParam, StringParam
+from countess import VERSION
+from countess.core.parameters import *
 from countess.core.plugins import DaskBasePlugin, DaskInputPlugin
 from countess.utils.dask import merge_dask_dataframes
 
-VERSION = "0.0.1"
-
+# XXX it would be better to do the same this Regex Tool does and get the user to assign
+# data types to each column
 
 def maybe_number(x):
     """CSV is never clear on if something is actually a number so ... try it I guess ..."""
@@ -42,33 +43,60 @@ class LoadCsvPlugin(DaskInputPlugin):
 
     parameters = {
         "header": BooleanParam("CSV file has header row?", True),
-        "prefix": StringParam("Column Name Prefix", ""),
+        "filename_column": StringParam("Filename Column", ""),
+        "columns": ArrayParam("Columns", MultiParam("Column", {
+            "name": StringParam("Column Name", ""),
+            "type": ChoiceParam("Column Type", "string", choices = ["string", "number", "integer", "boolean", "none"]),
+        }))
     }
 
-    file_params = {
-        "suffix": StringParam("Column Name Suffix", ""),
-    }
+    file_params = { }
 
     def read_file_to_dataframe(self, file_param, column_suffix='', row_limit=None):
      
         filename = file_param["filename"].value
 
+        def cast(value, datatype):
+            if datatype == 'string':
+                return str(value) if value is not None else None
+            elif datatype == 'number':
+                return float(value if value is not None else math.nan)
+            elif datatype == 'integer':
+                return int(value or 0)
+            elif datatype == 'boolean':
+                return bool(value) if value is not None else None
+            else:
+                return None
+
         options = {
             'blocksize': '32MB',
-            'header': 0 if self.parameters['header'].value else None
+            'header': 0 if self.parameters['header'].value else None,
+            'converters': dict([
+                (n, lambda v, t=pp["type"].value: cast(v, t))
+                for n, pp in enumerate(self.parameters["columns"])
+            ])
         }
         
         ddf = dd.read_csv(filename, **options)
 
         # XXX is this reading the whole file?  Is this bad?
+        # Dask doesn't support nrows, I think.
         if row_limit is not None:
             ddf = ddf.head(n=row_limit)
 
-        prefix = self.parameters["prefix"].value
-        suffix = file_param["suffix"].value
-        if prefix or suffix:
-            ddf.columns = [ prefix + str(colname) + suffix for colname in ddf.columns ]
- 
+        while len(ddf.columns) > len(self.parameters['columns']):
+            self.parameters['columns'].add_row()
+
+        for n, pp in enumerate(self.parameters["columns"]):
+            if pp["type"].value == 'none':
+                ddf = ddf.drop(columns=[ddf.columns[n]])
+            elif pp["name"].value and pp["name"].value != ddf.columns[n]:
+                ddf = ddf.rename(columns={ ddf.columns[n]: pp["name"].value})
+
+        filename_column = self.parameters['filename_column'].value
+        if filename_column:
+            ddf[filename_column] = filename
+
         return ddf
 
 class SaveCsvPlugin(DaskBasePlugin):
