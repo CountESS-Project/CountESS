3e613eda0c03adf0416788014cf019199f2a5291
Nick Moore <nick@zoic.org>
2023-04-13 16:53:38 +1000

    code cleanup

--- countess/gui/config.py
+++ countess/gui/config.py
@@ -80,7 +80,9 @@ class ParameterWrapper:
             self.var = tk.StringVar(tk_parent, value=parameter.value)
             self.entry = ttk.Entry(tk_parent, textvariable=self.var)
             self.entry.state(["readonly"])
-            self.button = tk.Button(tk_parent, text="Select", width=3, command=self.change_file_callback)
+            self.button = tk.Button(
+                tk_parent, text="Select", width=3, command=self.change_file_callback
+            )
         elif isinstance(parameter, TextParam):
             # tk.Text widget doesn't have a variable, for whatever reason,
             # so we use a different method!
@@ -338,6 +340,7 @@ class ParameterWrapper:
         file_types = self.parameter.file_types
         filename = filedialog.askopenfilename(filetypes=file_types)
         self.parameter.value = filename
+        self.var.set(self.parameter.value)
         self.callback(self.parameter)
 
     def set_value(self, value):
