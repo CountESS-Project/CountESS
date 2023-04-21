09eea82364d3507466580d6b34e8b91dfa0f22c1
Nick Moore <nick@zoic.org>
2023-04-21 15:31:21 +1000

    give variant plugin the option to work on the index

--- countess/plugins/variant.py
+++ countess/plugins/variant.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+from collections import Counter
+
 from countess import VERSION
 from countess.core.logger import Logger
-from countess.core.parameters import ColumnChoiceParam, IntegerParam, StringParam
+from countess.core.parameters import ColumnOrIndexChoiceParam, IntegerParam, StringParam
 from countess.core.plugins import PandasTransformPlugin
 from countess.utils.variant import find_variant_string
 
@@ -24,23 +26,37 @@ class VariantPlugin(PandasTransformPlugin):
     link = "https://countess-project.github.io/CountESS/plugins/#variant"
 
     parameters = {
-        "column": ColumnChoiceParam("Input Column"),
+        "column": ColumnOrIndexChoiceParam("Input Column"),
         "sequence": StringParam("Reference Sequence"),
-        "output": StringParam("Output Column"),
+        "output": StringParam("Output Column", "variant"),
         "max_mutations": IntegerParam("Max Mutations", 10),
     }
 
     def run_df(self, df: pd.DataFrame, logger: Logger) -> pd.DataFrame:
-        assert isinstance(self.parameters["column"], ColumnChoiceParam)
+        assert isinstance(self.parameters["column"], ColumnOrIndexChoiceParam)
         assert isinstance(self.parameters["sequence"], StringParam)
         assert isinstance(self.parameters["output"], StringParam)
         assert isinstance(self.parameters["max_mutations"], IntegerParam)
 
+        dfo = df.copy()
+
         output = self.parameters["output"].value
-        column = self.parameters["column"].value
+
+        if self.parameters["column"].is_index():
+            dfo['__index'] = df.index
+            column_name = '__index'
+        else:
+            column_name = self.parameters["column"].value
+
+        if self.parameters["sequence"].value == "":
+            self.parameters["sequence"].value = dfo[column_name][0]
+
         sequence = self.parameters["sequence"].value
         max_mutations = self.parameters["max_mutations"].value
 
-        dfo = df.copy()
-        dfo[output] = dfo[column].apply(process_row, args=(sequence, max_mutations, logger))
-        return dfo
+        dfo[output] = dfo[column_name].apply(process_row, args=(sequence, max_mutations, logger))
+
+        if self.parameters["column"].is_index():
+            return dfo.drop(columns=['__index'])
+        else:
+            return dfo
