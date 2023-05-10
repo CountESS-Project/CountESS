23b862053affa6e9a31270808045bdeeb3f0bcba
Nick Moore <nick@zoic.org>
2023-05-10 14:17:51 +1000

    Add 'python' (embedded python) and 'sequence' (sequence manipulation utils) plugins

---
+++ countess/plugins/python.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+from countess import VERSION
+from countess.core.logger import Logger
+from countess.core.parameters import TextParam
+from countess.core.plugins import PandasTransformPlugin
+
+
+# XXX pretty sure this is a job for ast.parse rather than just
+# running compile() and exec() but that can wait.
+
+class PythonPlugin(PandasTransformPlugin):
+    name = "Python Code"
+    description = """
+        Apply python code.  Columns are mapped to local variables and back.
+        "__index" is set to the index value.
+        If you assign to a variable called "__filter", only rows where that
+        value is True will be kept.
+    """
+    
+    version = VERSION
+
+    parameters = {"code": TextParam("Python Code")}
+
+    def run_df(self, df, logger: Logger) -> pd.DataFrame:
+        assert isinstance(self.parameters["code"], TextParam)
+        code_object = compile(self.parameters["code"].value, "<PythonPlugin>", mode="exec")
+
+        def _process(row):
+            row_dict = dict(row)
+            exec(code_object, {}, row_dict)  # pylint: disable=exec-used
+            return dict((k, v) for k, v in row_dict.items() if type(v) in (bool, int, float, str))
+
+        dfo = df.assign(__index=df.index)
+        dfo = dfo.apply(_process, axis=1, result_type="expand") 
+
+        if '__index' in dfo.columns:
+            dfo = dfo.drop(columns='__index')
+
+        if '__filter' in dfo.columns:
+            dfo = dfo.query('__filter').drop(columns='__filter')
+
+        return dfo
---
+++ countess/plugins/sequence.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from countess import VERSION
+from countess.core.logger import Logger
+from countess.core.parameters import BooleanParam, ColumnOrIndexChoiceParam, IntegerParam, StringParam, StringCharacterSetParam
+from countess.core.plugins import PandasTransformPlugin
+from countess.utils.variant import invert_dna_sequence
+
+
+class SequencePlugin(PandasTransformPlugin):
+    """Manipulate DNA sequences"""
+
+    name = "Sequence Tool"
+    description = "Manipulate DNA Sequences"
+    version = VERSION
+    link = "https://countess-project.github.io/CountESS/plugins/#sequence"
+
+    parameters = {
+        "column": ColumnOrIndexChoiceParam("Input Column"),
+        "invert": BooleanParam("Invert", False),
+        "offset": IntegerParam("Offset", 0),
+        "start": StringParam("Start at ...", ""),
+        "stop": StringParam("Stop at ...", ""),
+        "length": IntegerParam("Max Length", 150),
+        "output": StringParam("Output Column", "sequence"),
+    }
+
+    def run_df(self, df: pd.DataFrame, logger: Logger) -> pd.DataFrame:
+
+        def _process(seq):
+            if self.parameters["invert"].value:
+                seq = invert_dna_sequence(seq)
+            if self.parameters["offset"].value:
+                offset = self.parameters["offset"].value
+                seq = seq[offset:]
+            if self.parameters["start"].value:
+                offset = seq.find(self.parameters["start"].value)
+                if offset >= 0:
+                    seq = seq[offset:]
+                else:
+                    return None
+            if self.parameters["stop"].value:
+                offset = seq.find(self.parameters["stop"].value)
+                if offset >= 0:
+                    seq = seq[0:offset+len(self.parameters["stop"].value)]
+            if self.parameters["length"].value:
+                seq = seq[0:self.parameters["length"].value]
+            return seq
+
+        dfo = df.copy()
+        dfo[self.parameters["output"].value] = dfo[self.parameters["column"].value].apply(_process)
+        return dfo
