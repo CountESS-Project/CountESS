4d689490d22e51baf98e7393b1c54fafcd184cdf
Nick Moore <nick@zoic.org>
2023-07-08 07:27:40 +1000

    fixing join plugin

--- countess/plugins/join.py
+++ countess/plugins/join.py
@@ -12,6 +12,12 @@ from countess.core.plugins import PandasBasePlugin
 INDEX = "— INDEX —"
 
 
+def _join_how(left_required: bool, right_required: bool) -> str:
+    if left_required:
+        return "inner" if right_required else "left"
+    else:
+        return "right" if right_required else "outer"
+
 class JoinPlugin(PandasBasePlugin):
     """Joins Pandas Dataframes"""
 
@@ -35,86 +41,19 @@ class JoinPlugin(PandasBasePlugin):
         )
     }
 
-    def prepare(self, data, logger: Logger):
-        data_items = list(data.items())
-        inputs_param = self.parameters["inputs"]
-        assert isinstance(inputs_param, ArrayParam)
-
-        if len(data_items) != 2 or len(inputs_param) != 2:
-            raise NotImplementedError("Only two-way joins supported right now")
-        if not all((isinstance(df, pd.DataFrame) for _, df in data_items)):
-            raise NotImplementedError("Feed me dataframes")
-
-        enumerate_inputs = enumerate(zip(inputs_param, data.items()))
-        for number, (input_param, (source_name, source_ddf)) in enumerate_inputs:
-            input_param.label = f"Input {number+1}: {source_name}"
-            if hasattr(source_ddf.index, "names"):
-                input_columns = [n for n in source_ddf.index.names if n] + list(source_ddf.columns)
-            elif source_ddf.index.name:
-                input_columns = [source_ddf.index.name] + list(source_ddf.columns)
-            else:
-                input_columns = list(source_ddf.columns)
-            input_param["join_on"].choices = [INDEX] + input_columns
-
-    def run(
-        self,
-        data,
-        logger: Logger,
-        row_limit: Optional[int] = None,
-    ):
-        inputs_param = self.parameters["inputs"]
-        assert isinstance(inputs_param, ArrayParam)
-        assert len(inputs_param) == 2
-        assert isinstance(data, Mapping)
-        assert len(data) == 2
-        assert all(isinstance(d, pd.DataFrame) for d in data.values())
-
-        ip1, ip2 = inputs_param
-        df1, df2 = data.values()
-
-        if ip1.required.value and ip2.required.value:
-            join_how = "inner"
-        elif ip1.required.value:
-            join_how = "left"
-        elif ip2.required.value:
-            join_how = "right"
-        else:
-            join_how = "outer"
-
-        join_params: MutableMapping[str, Union[str, bool]] = {
-            "how": join_how,
-        }
-
+    def join_dataframes(self, dataframe1: pd.DataFrame, dataframe2: pd.DataFrame, join_params) -> pd.DataFrame:
         # "left_on" and "right_on" don't seem to mind if the column
         # is an index, but don't seem to work correctly if the column
         # is part of a multiindex: the other multiindex columns go missing.
+        join1 = join_params.get("left_on")
+        if join1 and join1 not in dataframe1.columns and dataframe1.index.name != join1:
+            dataframe1 = dataframe1.reset_index()
 
-        join1 = ip1.join_on.value
-        join2 = ip2.join_on.value
-
-        if join1 and join1 != INDEX:
-            if join1 not in df1.columns and df1.index.name != join1:
-                df1 = df1.reset_index()
-            join_params["left_on"] = join1
-        else:
-            join_params["left_index"] = True
-
-        if join2 and join2 != INDEX:
-            if join2 not in df2.columns and df2.index.name != join2:
-                df2 = df2.reset_index()
-            join_params["right_on"] = join2
-        else:
-            join_params["right_index"] = True
-
-        return df1.merge(df2, **join_params)
-
-    source1 = None
-    source2 = None
-    memo1 = []
-    memo2 = []
-
-    def join_dataframes(self, dataframe1: pd.DataFrame, dataframe2: pd.DataFrame) -> pd.DataFrame:
-        return dataframe1.join(dataframe2)
+        join2 = join_params.get("right_on")
+        if join2 and join2 not in dataframe2.columns and dataframe2.index.name != join2:
+            dataframe2 = dataframe2.reset_index()
+        
+        return dataframe1.merge(dataframe2, **join_params)
 
     def process_inputs(
         self, inputs: Mapping[str, Iterable[pd.DataFrame]], logger: Logger, row_limit: Optional[int]
@@ -124,7 +63,30 @@ class JoinPlugin(PandasBasePlugin):
         except ValueError:
             raise NotImplementedError("Only two-way joins implemented at this time")  # pylint: disable=raise-missing-from
 
+        inputs_param = self.parameters["inputs"]
+        assert isinstance(inputs_param, ArrayParam)
+        assert len(inputs_param) == 2
+        ip1, ip2 = inputs_param
+
+        join_params = {
+            "how": _join_how(ip1.required.value, ip2.required.value)
+        }
+        join1 = ip1.join_on.value
+        join2 = ip2.join_on.value
+
+        if join1 and join1 != INDEX:
+            join_params["left_on"] = join1
+        else:
+            join_params["left_index"] = True
+
+        if join2 and join2 != INDEX:
+            join_params["right_on"] = join2
+        else:
+            join_params["right_index"] = True
+
+        print(f"join_params {join_params}")
+
         for df_in1, df_in2 in product(input1, input2):
-            df_out = self.join_dataframes(df_in1, df_in2)
+            df_out = self.join_dataframes(df_in1, df_in2, join_params)
             if len(df_out):
                 yield df_out
