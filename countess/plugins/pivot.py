0dec1934d6d797b6d983f9aecda2b1a06be98fa0
Nick Moore <nick@zoic.org>
2023-09-22 15:52:09 +1000

    rewrite pivotplugin to work with new pipeline

--- countess/plugins/pivot.py
+++ countess/plugins/pivot.py
@@ -1,15 +1,18 @@
+from typing import Dict, List, Optional
+
 import numpy as np
 import pandas as pd
 
 from countess import VERSION
 from countess.core.logger import Logger
-from countess.core.parameters import ChoiceParam, PerColumnArrayParam
-from countess.core.plugins import PandasSimplePlugin
+from countess.core.parameters import ChoiceParam, PerColumnMultiParam
+from countess.core.plugins import PandasProcessPlugin
+from countess.utils.pandas import get_all_columns
 
 AGG_FUNCTIONS = ["first", "sum", "count", "mean"]
 
 
-class PivotPlugin(PandasSimplePlugin):
+class PivotPlugin(PandasProcessPlugin):
     """Groups a Pandas Dataframe by an arbitrary column and rolls up rows"""
 
     name = "Pivot Tool"
@@ -18,21 +21,36 @@ class PivotPlugin(PandasSimplePlugin):
     link = "https://countess-project.github.io/CountESS/plugins/#pivot-tool"
 
     parameters = {
-        "columns": PerColumnArrayParam("Columns", ChoiceParam("Role", choices=["Index", "Pivot", "Expand", "Drop"]))
+        "columns": PerColumnMultiParam("Columns", ChoiceParam("Role", choices=["Index", "Pivot", "Expand", "Drop"]))
     }
 
-    def process_dataframe(self, dataframe: pd.DataFrame, logger: Logger) -> pd.DataFrame:
-        assert isinstance(self.parameters["columns"], PerColumnArrayParam)
-        column_parameters = list(zip(self.input_columns, self.parameters["columns"]))
-        index_cols = [col for col, col_param in column_parameters if col_param.value == "Index"]
-        pivot_cols = [col for col, col_param in column_parameters if col_param.value == "Pivot"]
-        expand_cols = [col for col, col_param in column_parameters if col_param.value == "Expand"]
+    input_columns: Dict[str, np.dtype] = {}
 
-        # XXX friendly error messages please
-        assert pivot_cols
+    dataframes = []
+
+    def prepare(self, sources: List[str], row_limit: Optional[int]):
+        assert isinstance(self.parameters["columns"], PerColumnMultiParam)
+        # self.input_columns = {}
+
+        self.dataframes = []
+
+    def process(self, data: pd.DataFrame, source: str, logger: Logger):
+        self.input_columns.update(get_all_columns(data))
+
+        print(f"PTP {data}")
+
+        params_by_column_name = self.parameters["columns"].params.items()
+
+        index_cols = [col for col, param in params_by_column_name if param.value == "Index"]
+        pivot_cols = [col for col, param in params_by_column_name if param.value == "Pivot"]
+        expand_cols = [col for col, param in params_by_column_name if param.value == "Expand"]
+        print(f"XXX {params_by_column_name} {index_cols} {pivot_cols} {expand_cols}")
+
+        if not pivot_cols:
+            return []
 
         df = pd.pivot_table(
-            dataframe,
+            data,
             values=expand_cols,
             index=index_cols,
             columns=pivot_cols,
@@ -46,4 +64,17 @@ class PivotPlugin(PandasSimplePlugin):
                 "__".join([f"{cn}_{cv}" if cn else cv for cn, cv in zip(df.columns.names, cc)]) for cc in df.columns
             ]  # type: ignore
 
-        return df
+        self.dataframes.append(df)
+        return []
+
+    def finalize(self, logger: Logger):
+        params_by_column_name = self.parameters["columns"].params.items()
+        index_cols = [col for col, param in params_by_column_name if param.value == "Index"]
+        print(f"FINALIZE {self.dataframes} {index_cols}")
+        if self.dataframes:
+            df = pd.concat(self.dataframes).groupby(by=index_cols, group_keys=True).sum()
+            self.dataframes = []
+            yield df
+
+        for p in self.parameters.values():
+            p.set_column_choices(self.input_columns.keys())
