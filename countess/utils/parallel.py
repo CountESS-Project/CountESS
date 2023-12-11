a206d09920f150e1ddb17a6ac751be52eab337d8
Nick Moore <nick@zoic.org>
2023-12-11 15:02:31 +1100

    rework this parallel calling code ...

--- countess/utils/parallel.py
+++ countess/utils/parallel.py
@@ -3,40 +3,46 @@ import time
 from multiprocessing import Process, Queue, Value
 from os import cpu_count, getpid
 from queue import Empty
-from typing import Iterable
+from typing import Iterable, Callable, TypeVar, ParamSpec, Concatenate
 
 import psutil
 
 
+D = TypeVar('D')
+V = TypeVar('V')
+P = ParamSpec('P')
 
-def multi_iterator_map(function, values, args, progress_cb=None) -> Iterable:
+def multiprocess_map(function : Callable[Concatenate[V, P], D], values : Iterable[V],
+                     *args : P.args, **kwargs : P.kwargs) -> Iterable[D]:
     """Pretty much equivalent to:
-        interleave_longest(function(v, *args) for v in values)
-    but runs in multiple processes using a queue
-    to organize `values` and another queue to organize the
+
+        def multiprocess_map(function, values, *args, **kwargs): 
+            yield from interleave_longest(function(v, *args, **kwargs) for v in values)
+
+    but runs in multiple processes using an input_queue
+    to organize `values` and an output_queue to organize the
     returned values."""
 
+    # Start up several workers.
     nproc = ((cpu_count() or 1) + 1) // 2
-    queue1: Queue = Queue()
-    queue2: Queue = Queue(maxsize=3)
+    input_queue: Queue = Queue()
+    output_queue: Queue = Queue(maxsize=3)
 
-    # XXX do this a better way
-    len_values = [0]
-    yield_count = 0
-
-    if progress_cb:
-        progress_cb(0)
+    # XXX is it actually necessary to have this in a separate thread or
+    # would it be sufficient to add items to input_queue alternately with
+    # removing items from output_queue?
 
     enqueue_running = Value("b", True)
 
     def __enqueue():
         for v in values:
-            len_values[0] += 1
-            queue1.put(v)
+            input_queue.put(v)
         enqueue_running.value = False
 
     thread = threading.Thread(target=__enqueue)
     thread.start()
+
+    # XXX is this necessary?
     time.sleep(1)
 
     def __process():
@@ -50,9 +56,9 @@ def multi_iterator_map(function, values, args, progress_cb=None) -> Iterable:
                         print(f"{getpid()} LOW MEMORY {psutil.virtual_memory().percent}")
                         time.sleep(1)
 
-                    data_in = queue1.get(timeout=1)
-                    for data_out in function(data_in, *args):
-                        queue2.put(data_out)
+                    data_in = input_queue.get(timeout=1)
+                    for data_out in function(data_in, *args, **(kwargs or {})):
+                        output_queue.put(data_out)
 
                         # Make sure large data is disposed of before we
                         # go around for the next loop
@@ -69,12 +75,6 @@ def multi_iterator_map(function, values, args, progress_cb=None) -> Iterable:
 
     while thread.is_alive() or any(p.is_alive() for p in processes):
         try:
-            yield queue2.get(timeout=1)
-            yield_count += 1
+            yield output_queue.get(timeout=0.1)
         except Empty:
             pass
-        if progress_cb and len_values[0]:
-            progress_cb((100 * yield_count) // len_values[0])
-
-    if progress_cb:
-        progress_cb(100)
