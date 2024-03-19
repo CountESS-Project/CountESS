c74a07164a459cd5b44419c999f3c229c228bcab
Nick Moore <nick@zoic.org>
2024-03-19 13:04:16 +1100

    Another attempt at fixing #29

--- countess/gui/unicode.py
+++ countess/gui/unicode.py
@@ -1,8 +1,21 @@
 import tkinter as tk
+import tkinter.font as tk_font
 
-# Tcl support for unicode starts in version 8.1, April 1999.
+# We found a shared remote system where these unicode icons are
+# displayed as escaped strings, literally "\u2795" on what should
+# be the "+" button.  This is very confusing, so as a workaround
+# check if the string "\u2795" (one unicode codepoint) renders over
+# three em wide, in which case the Unicode support is fonts are broken.
 
-if tk.TclVersion < 8.1:
+def unicode_is_broken():
+    root = tk.Tk()
+    font = tk_font.Font(root)
+    is_broken = font.measure("\u2795") > 3 * font.measure("m")
+    root.destroy()
+    return is_broken
+
+
+if unicode_is_broken():
     UNICODE_CHECK = "Y"
     UNICODE_UNCHECK = "N"
     UNICODE_CROSS = "X"
