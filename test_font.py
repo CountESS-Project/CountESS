f24460be0e9a86427f454e00959c93ed80b09103
Nick Moore <nick@zoic.org>
2024-03-21 15:12:23 +1100

    .........

--- test_font.py
+++ test_font.py
@@ -4,5 +4,6 @@ import tkinter.font as tk_font
 root = tk.Tk()
 font = tk_font.Font()
 
-print(font.measure("\u2795"))
+print(font.measure("n"))
 print(font.measure("m"))
+print(font.measure("\u2795"))
