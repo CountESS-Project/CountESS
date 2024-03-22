7fce6a70938c2cb0485f60a7c518e6ddf3766688
Nick Moore <nick@zoic.org>
2024-03-22 13:41:08 +1100

    Switch from unicode icons to GIF icons.

--- countess/gui/widgets.py
+++ countess/gui/widgets.py
@@ -1,61 +1,45 @@
 import tkinter as tk
-import tkinter.font as tk_font
 from functools import cache
 from typing import Optional
+from importlib.resources import files, as_file
 
 
 @cache
-def unicode_is_broken() -> bool:
-    """Some (older?) installations seem to have broken unicode.
-    The buttons display a unicode escape string instead of a
-    unicode symbol.  So this detects if that is happening and
-    the buttons call then fall back to ASCII characters."""
-
-    root = tk.Tk()
-    font = tk_font.Font(root)
-    width_1 = font.measure("m")
-    width_2 = font.measure("\u2795")
-    root.destroy()
-
-    return width_2 > 3 * width_1
+def get_bitmap_image(name: str) -> tk.PhotoImage:
+    source = files('countess.gui').joinpath(f"icons/{name}.gif")
+    with as_file(source) as filepath:
+        return tk.PhotoImage(file=filepath)
 
 
 def info_button(parent: tk.Widget, *args, **kwargs) -> tk.Button:
-    kwargs["text"] = "i" if unicode_is_broken() else "\u2139"
-    kwargs["fg"] = "blue"
+    kwargs["image"] = get_bitmap_image("info")
     return tk.Button(parent, *args, **kwargs)
 
 
 def add_button(parent: tk.Widget, *args, **kwargs) -> tk.Button:
-    kwargs["text"] = "+" if unicode_is_broken() else "\u2795"
-    kwargs["width"] = 2
+    kwargs["image"] = get_bitmap_image("add")
     return tk.Button(parent, *args, **kwargs)
 
 
 def delete_button(parent: tk.Widget, *args, **kwargs) -> tk.Button:
-    kwargs["text"] = "X" if unicode_is_broken() else "\u2715"
-    kwargs["width"] = 2
+    kwargs["image"] = get_bitmap_image("del")
     return tk.Button(parent, *args, **kwargs)
 
 
 class BooleanCheckbox(tk.Button):
     def __init__(self, *args, **kwargs):
-        kwargs["width"] = 2
         super().__init__(*args, **kwargs)
 
     def set_value(self, value: Optional[bool]):
         if value is None:
-            self["text"] = ""
-            self["fg"] = self["bg"]
+            self["image"] = None
             self["state"] = tk.DISABLED
             self["bd"] = 0
         elif value:
-            self["text"] = "Y" if unicode_is_broken() else "\u2714"
-            self["fg"] = "black"
+            self["image"] = get_bitmap_image("check")
             self["state"] = tk.NORMAL
             self["bd"] = 1
         else:
-            self["text"] = "N" if unicode_is_broken() else "\u2717"
-            self["fg"] = "grey"
+            self["image"] = get_bitmap_image("uncheck")
             self["state"] = tk.NORMAL
             self["bd"] = 1
