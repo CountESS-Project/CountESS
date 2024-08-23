ae59704de22effa2446f2942d1681ff62b6d1000
Nick Moore <nick@zoic.org>
2024-08-23 17:17:22 +1000

    add tests for components

---
+++ tests/gui/test_mini_browser.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+import pytest
+import tkinterweb
+
+from countess.gui.main import make_root
+from countess.gui.mini_browser import MiniBrowserFrame
+
+example_url = "http://example.com/"
+
+@pytest.mark.gui
+def test_mini_browser():
+    with patch.object(tkinterweb.HtmlFrame, 'load_url') as p:
+
+        root = make_root()
+        MiniBrowserFrame(root, example_url)
+
+        p.assert_called_once()
---
+++ tests/utils/test_parallel.py
@@ -0,0 +1,13 @@
+from countess.utils.parallel import multiprocess_map
+
+def test_multiprocess_map():
+
+    def function(value):
+        for n in range(0,value):
+            yield (n + 1) * value
+
+    values = [ 1, 2, 3, 4, 5, 6, 7 ]
+
+    output = multiprocess_map(function, values)
+
+    assert sorted(output) == [ 1,2,3,4,4,5,6,6,7,8,9,10,12,12,14,15,16,18,20,21,24,25,28,30,35,36,42,49 ]
