09e37de1d9775dd9bc7c9b300dacc2c39bf98252
Nick Moore <nick@zoic.org>
2024-08-29 10:19:00 +1000

    make logger setup for countess_cmd a bit more readable

--- countess/core/cmd.py
+++ countess/core/cmd.py
@@ -17,15 +17,28 @@ def run(argv) -> None:
 
 
 def main() -> None:
+    # set up a default stderr StreamHandler for logs
+    logging_handler = logging.StreamHandler()
+
+    # set up a QueueHandler/QueueListener to forward the logs between
+    # processes and send them to the logging_handler
     logging_queue: multiprocessing.Queue = multiprocessing.Queue()
-    logging.getLogger().addHandler(logging.handlers.QueueHandler(logging_queue))
-    logging.getLogger().setLevel(logging.INFO)
-    logging_handler = logging.handlers.QueueListener(logging_queue, logging.StreamHandler())
-    logging_handler.start()
+    logging_queue_handler = logging.handlers.QueueHandler(logging_queue)
+    logging_queue_listener = logging.handlers.QueueListener(logging_queue, logging_handler)
+    logging_queue_listener.start()
+
+    # set up all loggers to be handled by the QueueHandler.
+    root_logger = logging.getLogger()
+    root_logger.addHandler(logging_queue_handler)
+    root_logger.setLevel(logging.INFO)
 
     run(sys.argv[1:])
 
-    logging_handler.stop()
+    # shut down the logging subsystem, in case this function is being
+    # called as part of something else (eg: tests)
+    root_logger.handlers.clear()
+    logging_queue_listener.stop()
+    logging_queue.close()
 
 
 if __name__ == "__main__":
