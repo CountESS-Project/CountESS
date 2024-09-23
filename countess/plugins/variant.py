a71fb409777c66c5aef61171e37376b628922b23
Nick Moore <nick@zoic.org>
2024-09-23 13:35:24 +1000

    new variant caller gets it right!

--- countess/plugins/variant.py
+++ countess/plugins/variant.py
@@ -1,15 +1,13 @@
 import logging
-from typing import Optional
-
-import pandas as pd
 
 from countess import VERSION
 from countess.core.parameters import (
-    BooleanParam,
+    ArrayParam,
     ColumnChoiceParam,
     ColumnOrIntegerParam,
     ColumnOrStringParam,
     IntegerParam,
+    MultiParam,
     StringParam,
 )
 from countess.core.plugins import PandasTransformDictToDictPlugin
@@ -18,89 +16,48 @@ from countess.utils.variant import find_variant_string
 logger = logging.getLogger(__name__)
 
 
+class VariantOutputMultiParam(MultiParam):
+    prefix = StringParam("Prefix", "g.")
+    offset = ColumnOrIntegerParam("Offset", 0)
+    maxlen = IntegerParam("Max Variations", 10)
+    output = StringParam("Output Column", "variant")
+
+
 class VariantPlugin(PandasTransformDictToDictPlugin):
     """Turns a DNA sequence into a HGVS variant code"""
 
-    name = "Variant Translator"
+    name = "Variant Caller"
     description = "Turns a DNA sequence into a HGVS variant code"
     version = VERSION
     link = "https://countess-project.github.io/CountESS/included-plugins/#variant-caller"
+    additional = """Prefixes should end with 'g.', 'c.' or 'p.'.
+    Use a negative offset to call genomic references to minus-strand genes."""
 
     column = ColumnChoiceParam("Input Column", "sequence")
     reference = ColumnOrStringParam("Reference Sequence")
-    minus_strand = BooleanParam("Reference is on Minus Strand", False)
-    offset = ColumnOrIntegerParam("Reference Offset", 0)
-    prefix = StringParam("Output Prefix", "")
-    output = StringParam("Output Column", "variant")
-
-    coding_prefix = StringParam("Coding Prefix", "c.")
-    coding_offset = ColumnOrIntegerParam("Coding Offset", 0)
-    coding_output = StringParam("Coding Output", "coding")
-
-    max_mutations = IntegerParam("Max Mutations", 10)
-    protein = StringParam("Protein Column", "protein")
-    max_protein = IntegerParam("Max Protein Variations", 10)
-    drop = BooleanParam("Drop unidentified variants", False)
-    drop_columns = BooleanParam("Drop Input Column(s)", False)
+    outputs = ArrayParam("Outputs", VariantOutputMultiParam("Output"))
 
     def process_dict(self, data) -> dict:
         sequence = data[str(self.column)]
+        reference = self.reference.get_value_from_dict(data)
         if not sequence:
             return {}
 
-        reference = self.reference.get_value_from_dict(data)
-        offset = int(self.offset.get_value_from_dict(data) or 0)
-        coding_offset = int(self.coding_offset.get_value_from_dict(data) or 0)
-
         r: dict[str, str] = {}
+        for output in self.outputs:
+            offset = int(output.offset.get_value_from_dict(data) or 0)
 
-        try:
-            if self.output:
-                r[self.output.value] = find_variant_string(
-                    self.prefix + ":g." if self.prefix else "g.",
+            try:
+                r[output.output.value] = find_variant_string(
+                    str(output.prefix) if output.prefix else "g.",
                     reference,
                     sequence,
-                    max_mutations=self.max_mutations.value,
-                    offset=offset,
-                    minus_strand=self.minus_strand.value,
+                    max_mutations=output.maxlen.value,
+                    offset=abs(offset),
+                    minus_strand=offset < 0,
                 )
 
-            if self.coding_output:
-                r[self.coding_output.value] = find_variant_string(
-                    self.coding_prefix + ":c." if self.coding_prefix else "c.",
-                    reference,
-                    sequence,
-                    int(self.max_mutations),
-                    offset=coding_offset,
-                )
-
-            if self.protein:
-                r[str(self.protein)] = find_variant_string(
-                    "p.", reference, sequence, int(self.max_protein), offset=coding_offset
-                )
-
-        #except ValueError:
-        #    pass
-        except (TypeError, KeyError, IndexError) as exc:
-            logger.warning("Exception", exc_info=exc)
+            except (TypeError, KeyError, IndexError) as exc:
+                logger.warning("Exception", exc_info=exc)
 
         return r
-
-    def process_dataframe(self, dataframe: pd.DataFrame) -> Optional[pd.DataFrame]:
-        df_out = super().process_dataframe(dataframe)
-
-        if df_out is not None:
-            if self.drop:
-                if self.output:
-                    df_out.dropna(subset=str(self.output), inplace=True)
-                if self.protein:
-                    df_out.dropna(subset=str(self.protein), inplace=True)
-            if self.drop_columns:
-                try:
-                    df_out.drop(columns=str(self.column), inplace=True)
-                    if self.reference.get_column_name():
-                        df_out.drop(columns=self.reference.get_column_name(), inplace=True)
-                except KeyError:
-                    pass
-
-        return df_out
