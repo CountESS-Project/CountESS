9999682fdf317830192507be43bc126c5a10a36b
Nick Moore <nick@zoic.org>
2025-02-21 15:18:26 +1100

    correlation tool (only Pearson's so far)

--- countess/plugins/correlation.py
+++ countess/plugins/correlation.py
@@ -1,50 +1,37 @@
-from typing import Iterable, Optional
+from typing import Optional
 
 import pandas as pd
+from duckdb import DuckDBPyConnection, DuckDBPyRelation
 
 from countess import VERSION
-from countess.core.parameters import ChoiceParam, ColumnChoiceParam, ColumnOrNoneChoiceParam
-from countess.core.plugins import PandasSimplePlugin
+from countess.core.parameters import ColumnChoiceParam, ColumnOrNoneChoiceParam
+from countess.core.plugins import DuckdbSimplePlugin
+from countess.utils.duckdb import duckdb_escape_identifier
 
 
-class CorrelationPlugin(PandasSimplePlugin):
+class CorrelationPlugin(DuckdbSimplePlugin):
     """Correlations"""
 
     name = "Correlation Tool"
-    description = "Measures Pearsons / Kendall / Spearman correlation of columns"
+    description = "Measures Pearsons r² correlation of columns"
     version = VERSION
     link = "https://countess-project.github.io/CountESS/included-plugins/#correlation-tool"
 
-    method = ChoiceParam("Method", choices=["pearson", "kendall", "spearman"])
     group = ColumnOrNoneChoiceParam("Group")
-    column1 = ColumnChoiceParam("Column 1")
-    column2 = ColumnChoiceParam("Column 2")
+    column1 = ColumnChoiceParam("Column X")
+    column2 = ColumnChoiceParam("Column Y")
 
     columns: list[str] = []
     dataframes: list[pd.DataFrame] = []
 
-    def prepare(self, sources: list[str], row_limit: Optional[int] = None):
-        column1 = self.column1.value
-        column2 = self.column2.value
-        self.columns = [column1, column2]
+    def execute(self, ddbc: DuckDBPyConnection, source: DuckDBPyRelation) -> Optional[DuckDBPyRelation]:
+
+        col1 = duckdb_escape_identifier(self.column1.value)
+        col2 = duckdb_escape_identifier(self.column2.value)
+
+        aggregate = f"regr_r2({col2},{col1}) AS regr_r2, regr_slope({col2},{col1}) AS regr_slope"
+
         if self.group.is_not_none():
-            self.columns.append(self.group.value)
-        self.dataframes = []
+            aggregate = duckdb_escape_identifier(self.group.value) + ", " + aggregate
 
-    def process_dataframe(self, dataframe: pd.DataFrame) -> None:
-        self.dataframes.append(dataframe[self.columns])
-
-    def finalize(self) -> Iterable[pd.DataFrame]:
-        column1 = self.column1.value
-        column2 = self.column2.value
-        groupby = None if self.group.is_none() else self.group.value
-
-        method = self.method.value
-
-        dataframe = pd.concat(self.dataframes)
-        if groupby:
-            ds = dataframe.groupby(groupby)[column1]
-            yield ds.corr(dataframe[column2], method=method).to_frame(name="correlation")
-        else:
-            corr = dataframe[column1].corr(dataframe[column2], method=method)
-            yield pd.DataFrame([[corr]], columns=["correlation"])
+        return source.aggregate(aggregate)
