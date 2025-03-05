7d19c1d99a22beb32040de465872906a1552c3ea
Nick Moore <nick@zoic.org>
2025-03-05 14:54:05 +1100

    percolumn approach

--- countess/plugins/vampseq.py
+++ countess/plugins/vampseq.py
@@ -9,6 +9,7 @@ from countess.core.parameters import (
     ColumnChoiceParam,
     FloatParam,
     TabularMultiParam,
+    PerColumnArrayParam,
 )
 from countess.core.plugins import DuckdbSimplePlugin
 from countess.utils.duckdb import duckdb_escape_identifier, duckdb_escape_literal, duckdb_dtype_is_numeric
@@ -17,7 +18,6 @@ logger = logging.getLogger(__name__)
 
 
 class CountColumnParam(TabularMultiParam):
-    col = ColumnChoiceParam("Column")
     weight = FloatParam("Weight")
 
 class VampSeqScorePlugin(DuckdbSimplePlugin):
@@ -25,23 +25,33 @@ class VampSeqScorePlugin(DuckdbSimplePlugin):
     description = "Calculate scores from weighed bin counts"
     version = VERSION
 
-    count_columns = ArrayParam("Count Columns", CountColumnParam("Count"), min_size=2)
+    columns = PerColumnArrayParam("Columns", CountColumnParam("Column"))
+
+    def prepare(self, ddbc: DuckDBPyConnection, source: Optional[DuckDBPyRelation]) -> None:
+        # Override prepare to only select numeric columns.
+        if source is None:
+            self.set_column_choices([])
+        else:
+            self.set_column_choices([
+                c for c, d in zip(source.columns, source.dtypes)
+                if duckdb_dtype_is_numeric(d)
+            ])
 
     def execute(
         self, ddbc: DuckDBPyConnection, source: DuckDBPyRelation, row_limit: Optional[int] = None
     ) -> Optional[DuckDBPyRelation]:
 
         weighted_columns = {
-            duckdb_escape_identifier(c.col.value): duckdb_escape_literal(c.weight.value)
-            for c in self.count_columns
-            if c.col.value in source.columns and duckdb_dtype_is_numeric(source[c.col.value].dtypes[0]) and c.weight.value is not None
+            duckdb_escape_identifier(name): duckdb_escape_literal(param.weight.value)
+            for name, param in self.columns.get_column_params()
+            if param.weight.value is not None
         }
+
+        if not weighted_columns:
+            return source
 
         weighted_counts = ' + '.join(f"{k} * {v}" for k, v in weighted_columns.items())
         total_counts = ' + '.join(k for k in weighted_columns.keys())
-
-        if not weighted_counts:
-            return source
 
         proj = f"*, ({weighted_counts}) / ({total_counts}) as score"
 
