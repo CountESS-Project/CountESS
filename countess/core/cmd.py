260cb41b30ed4c9b00c6f922dad437d729a84773
Nick Moore <nick@zoic.org>
2025-05-16 12:57:00 +1000

    simplify countess_cmd logging setup

--- countess/core/cmd.py
+++ countess/core/cmd.py
@@ -5,6 +5,7 @@ import logging.handlers
 import multiprocessing
 import re
 import sys
+import time
 from typing import Iterable, Union
 
 from countess import VERSION
@@ -59,8 +60,16 @@ def configure_graphs(args: list[str]) -> Iterable[PipelineGraph]:
             except ValueError:
                 logger.error("Bad --log level: %s", opt_val)
 
+    if not args:
+        logger.info("No config files found")
+
     for filename in args:
-        graph = read_config([filename])
+        try:
+            graph = read_config([filename])
+        except IOError as exc:
+            logger.warning("Error reading %s: %s", filename, exc)
+            continue
+
         for node_name, config_key, config_val in config:
             node = graph.find_node(node_name)
             if node:
@@ -80,29 +89,11 @@ def run(args: list[str]) -> None:
 
 
 def main() -> None:
-    # set up a default stderr StreamHandler for logs
-    logging_handler = logging.StreamHandler()
-
-    # set up a QueueHandler/QueueListener to forward the logs between
-    # processes and send them to the logging_handler
-    logging_queue: multiprocessing.Queue = multiprocessing.Queue()
-    logging_queue_handler = logging.handlers.QueueHandler(logging_queue)
-    logging_queue_listener = logging.handlers.QueueListener(logging_queue, logging_handler)
-    logging_queue_listener.start()
-
-    # set up all loggers to be handled by the QueueHandler.
     root_logger = logging.getLogger()
-    root_logger.addHandler(logging_queue_handler)
+    root_logger.addHandler(logging.StreamHandler())
     root_logger.setLevel(logging.INFO)
 
     run(sys.argv[1:])
-
-    # shut down the logging subsystem, in case this function is being
-    # called as part of something else (eg: tests)
-    root_logger.handlers.clear()
-    logging_queue_listener.stop()
-    logging_queue.close()
-
 
 if __name__ == "__main__":
     main()  # pragma: no cover
